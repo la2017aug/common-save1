b2e57f45c16ec12feba980574a2fd8ef6850a679
Oleksandr Lavrynenko <55446919+la2017aug@users.noreply.github.com>
2019-10-20 12:15:00 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,15 @@
 import unittest
-import math
+from math import sqrt
 
 from homework import Rectangle
 
 
 class RectangleTestCase(unittest.TestCase):
-    rectangle1 = Rectangle(2, 2)
-    rectangle2 = Rectangle(4, 2)
-    rectangle3 = Rectangle(2, 4)
+    def setUp(self):
+        self.rectangle1 = Rectangle(2, 2)
+        self.rectangle2 = Rectangle(4, 2)
+        self.rectangle3 = Rectangle(2, 4)
+        self.rectangle4 = Rectangle(2.2, 2.2)
 
     def test_get_rectangle_perimeter(self):
         self.assertEqual(self.rectangle1.get_rectangle_perimeter(), 8)
@@ -33,27 +35,30 @@ class RectangleTestCase(unittest.TestCase):
 
     def test_get_rectangle_diagonal(self):
         self.assertEqual(self.rectangle1.get_rectangle_diagonal(),
-                         math.sqrt(8))
+                         sqrt(8))
         self.assertEqual(self.rectangle2.get_rectangle_diagonal(),
-                         math.sqrt(20))
+                         sqrt(20))
         self.assertEqual(self.rectangle3.get_rectangle_diagonal(),
-                         math.sqrt(20))
+                         sqrt(20))
 
     def test_get_radius_of_circumscribed_circle(self):
         self.assertEqual(
             self.rectangle1.get_radius_of_circumscribed_circle(),
-            math.sqrt(8) / 2)
+            sqrt(8) / 2)
         self.assertEqual(
             self.rectangle2.get_radius_of_circumscribed_circle(),
-            math.sqrt(20) / 2)
+            sqrt(20) / 2)
         self.assertEqual(
             self.rectangle3.get_radius_of_circumscribed_circle(),
-            math.sqrt(20) / 2)
+            sqrt(20) / 2)
 
     def test_get_radius_of_inscribed_circle(self):
         self.assertEqual(
             self.rectangle1.get_radius_of_inscribed_circle(),
-            math.sqrt(8) / (2 * math.sqrt(2)))
+            sqrt(pow(2, 2) + pow(2, 2)) / (2 * sqrt(2)))
+        self.assertEqual(
+            self.rectangle4.get_radius_of_inscribed_circle(),
+            sqrt(pow(2.2, 2) + pow(2.2, 2)) / (2 * sqrt(2)))
         with self.assertRaises(ValueError):
             self.rectangle2.get_radius_of_inscribed_circle()
             self.rectangle3.get_radius_of_inscribed_circle()
