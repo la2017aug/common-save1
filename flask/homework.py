997d0eb5ae5a5d6b16d476fc7cb460863a241ca9
alex lavrinenko <la2017aug@gmail.com>
2019-10-29 13:40:53 +0200

    fixed

--- flask/homework.py
+++ flask/homework.py
@@ -10,13 +10,13 @@ def home():
 
 @app.route('/vegetables')
 def vegetables():
-    vegetables_list = ['beans', 'carrot', 'beetroot', 'cucumber'] #if you never gonna change this list in your code, use tuple
+    vegetables_list = ('beans', 'carrot', 'beetroot', 'cucumber')
     return render_template('vegetables.html', vegetables=vegetables_list)
 
 
 @app.route('/fruits')
 def fruits():
-    fruits_list = ['melon', 'apple', 'strawberry', 'grape']
+    fruits_list = ('melon', 'apple', 'strawberry', 'grape')
     return render_template('fruits.html', fruits=fruits_list)
 
 
