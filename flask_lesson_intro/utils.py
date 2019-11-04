3d8cd560c12c561b57136b55891109b532758aad
alex lavrinenko <la2017aug@gmail.com>
2019-11-04 19:26:01 +0200

    err test2

--- flask_lesson_intro/utils.py
+++ flask_lesson_intro/utils.py
@@ -5,6 +5,5 @@ def get_data():
     try:
         with open("data.json") as file:
             return json.load(file)
-    except(IOError, json.JSONDecodeError):
-        pass
-        #raise
+    except ValueError:
+        raise Exception('data.json is inconsistent or missing')
